07907d2be9ce527797ad3a81d4fed287605cdc35
ustazeynep <zeynep.n.usta@outlook.com>
2023-02-23 13:43:40 +0000

    class Day4 renamed as Test01

--- src/test/java/Day4/GitPractice/Test01.java
+++ src/test/java/Day4/GitPractice/Test01.java
@@ -1,9 +1,9 @@
 package Day4.GitPractice;
 
-public class Day4 {
+public class Test01 {
 
     public static void main(String[] args) {
-        System.out.println("This is day 4");
+        System.out.println("This is Test01");
     }
 
 }
